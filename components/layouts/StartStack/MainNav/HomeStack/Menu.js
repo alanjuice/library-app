90ecb047669373f7a242ace9641d6aed1293bb54
Alan <93069058+alanjuice@users.noreply.github.com>
2024-03-25 13:40:42 +0530

    display teacher name

--- components/layouts/StartStack/MainNav/HomeStack/Menu.js
+++ components/layouts/StartStack/MainNav/HomeStack/Menu.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   TouchableOpacity,
   Text,
   Image,
-  StatusBar,
+  ActivityIndicator,
   StyleSheet,
 } from "react-native";
 import { Appbar, Avatar } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
 import * as Animatable from "react-native-animatable";
+import * as SecureStore from "expo-secure-store";
+import jwtDecode from "jwt-decode";
 
 const BoxComponent = ({ text, imageSource, backgroundColor }) => (
-  <Animatable.View animation="fadeIn" duration={1000} style={[styles.box, { backgroundColor }]}>
+  <Animatable.View
+    animation="fadeIn"
+    duration={1000}
+    style={[styles.box, { backgroundColor }]}
+  >
     <Image source={imageSource} style={styles.boxImage} />
     <Text>{text}</Text>
   </Animatable.View>
@@ -20,69 +26,101 @@ const BoxComponent = ({ text, imageSource, backgroundColor }) => (
 
 function Menu() {
   const navigation = useNavigation();
+  const [loading, setLoading] = useState(true);
+  const [name, setName] = useState(""); // State to store name
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        const token = await SecureStore.getItemAsync("token");
+        const response = await fetch(
+          "https://sunday-library.onrender.com/teacher/details",
+          {
+            method: "GET",
+            headers: {
+              "x-authtoken": token,
+            },
+          }
+        );
+        const teacher = await response.json();
+        setName(teacher.name);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching or decoding token:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchToken();
+  }, []);
 
   return (
     <View style={{ flex: 1 }}>
       <View style={styles.mainContainer}>
         <Appbar style={styles.appbar}>
-          <Text style={styles.appbarText}>welcome teacher name</Text>
+          <Text style={styles.appbarText}>Welcome {loading ? "" : name}</Text>
           <Avatar.Image
             size={50}
             source={require("../../../../../assets/pfp.png")}
             style={styles.avatar}
           />
-        </Appbar>   
-        
-        <View style={styles.container}>
-          <View style={styles.columnContainer}>
-            <TouchableOpacity
-              style={styles.columnContainer}
-              onPress={() => {
-                navigation.navigate("Students");
-              }}
-            >
-              <BoxComponent
-                text="Students"
-                imageSource={require("../../../../../assets/students.png")}
-                backgroundColor="#AACCFF" // Specify color for the box
-              />
-            </TouchableOpacity>
+        </Appbar>
 
-            <TouchableOpacity
-              style={styles.columnContainer}
-              onPress={() => navigation.navigate("Allocate")}
-            >
-              <BoxComponent
-                text="Allocate"
-                imageSource={require("../../../../../assets/allocate.png")}
-                backgroundColor="#00B3FF" // Specify color for the box
-              />
-            </TouchableOpacity>
-          </View>
+        {loading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          />
+        ) : (
+          // Display content once loading is false
+          <View style={styles.container}>
+            <View style={styles.columnContainer}>
+              <TouchableOpacity
+                style={styles.columnContainer}
+                onPress={() => navigation.navigate("Students")}
+              >
+                <BoxComponent
+                  text="Students"
+                  imageSource={require("../../../../../assets/students.png")}
+                  backgroundColor="#AACCFF" // Specify color for the box
+                />
+              </TouchableOpacity>
 
-          <View style={styles.columnContainer}>
-            <TouchableOpacity
-              style={styles.columnContainer}
-              onPress={() => {
-                navigation.navigate("Books");
-              }}
-            >
-              <BoxComponent
-                text="Books"
-                imageSource={require("../../../../../assets/books.png")}
-                backgroundColor="#6FD6FF"
-                 // Specify color for the box
-              />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.columnContainer}>
-              <BoxComponent
-                text="Analytics"
-                imageSource={require("../../../../../assets/analytics.png")}
-                backgroundColor="#8AC8FF" // Specify color for the box
-              />
-            </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.columnContainer}
+                onPress={() => navigation.navigate("Allocate")}
+              >
+                <BoxComponent
+                  text="Allocate"
+                  imageSource={require("../../../../../assets/allocate.png")}
+                  backgroundColor="#00B3FF" // Specify color for the box
+                />
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.columnContainer}>
+              <TouchableOpacity
+                style={styles.columnContainer}
+                onPress={() => navigation.navigate("Books")}
+              >
+                <BoxComponent
+                  text="Books"
+                  imageSource={require("../../../../../assets/books.png")}
+                  backgroundColor="#6FD6FF"
+                  // Specify color for the box
+                />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.columnContainer}>
+                <BoxComponent
+                  text="Analytics"
+                  imageSource={require("../../../../../assets/analytics.png")}
+                  backgroundColor="#8AC8FF" // Specify color for the box
+                />
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
+        )}
       </View>
     </View>
   );
@@ -90,8 +128,8 @@ function Menu() {
 
 const styles = StyleSheet.create({
   appbar: {
-   backgroundColor: "#ffff",
-   color:'black',
+    backgroundColor: "#ffff",
+    color: "black",
     height: 64,
     flexDirection: "row",
     position: "absolute",
@@ -103,7 +141,7 @@ const styles = StyleSheet.create({
   },
   appbarText: {
     color: "white",
-    color:'black',
+    color: "black",
     fontSize: 25,
     fontWeight: "700",
   },
@@ -124,19 +162,18 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 20, // Border radius at the top right
     borderBottomLeftRadius: 0, // No border radius at the bottom left
     borderBottomRightRadius: 0, // No border radius at the bottom right
-   
+
     borderColor: "#e6e6e6",
-    bottom:-1
-    
+    bottom: -1,
   },
-  
+
   main: {
     flex: 1,
     backgroundColor: "#0D08F3",
     alignItems: "center",
     justifyContent: "center",
   },
-  
+
   boxImage: {
     width: 70,
     height: 70,
